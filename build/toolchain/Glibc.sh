c4df95df0fa0aa2a82b10fe5914d111f18a8dcd3
icodeinpython <99908587+icodeinpython@users.noreply.github.com>
2022-09-13 08:54:01 -0400

    Create Glibc.sh

---
+++ build/toolchain/Glibc.sh
@@ -0,0 +1,22 @@
+case $(uname -m) in
+    i?86)   ln -sfv ld-linux.so.2 $LFS/lib/ld-lsb.so.3
+    ;;
+    x86_64) ln -sfv ../lib/ld-linux-x86-64.so.2 $LFS/lib64
+            ln -sfv ../lib/ld-linux-x86-64.so.2 $LFS/lib64/ld-lsb-x86-64.so.3
+    ;;
+esac
+
+patch -Np1 -i ../glibc-2.32-fhs-1.patch
+
+mkdir -v build
+cd       build
+
+../configure                             \
+      --prefix=/usr                      \
+      --host=$LFS_TGT                    \
+      --build=$(../scripts/config.guess) \
+      --enable-kernel=3.2                \
+      --with-headers=$LFS/usr/include    \
+      libc_cv_slibdir=/lib
+make
+make DESTDIR=$LFS install
